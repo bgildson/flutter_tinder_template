be1567845ddac59cb1c0cdf0884c1f50623a0e05
bgildson <gildson13@hotmail.com>
2018-04-08 12:17:20 -0300

    implementing new characteristics and interactions in the icon rounded button widget

--- lib/ui/rounded_icon_button.dart
+++ lib/ui/rounded_icon_button.dart
@@ -5,26 +5,72 @@ class RoundedIconButton extends StatefulWidget {
   RoundedIconButton({
     Key key,
     @required this.icon,
-    this.color = Colors.black12,
-    this.iconColor = Colors.black26,
-    this.activeColor = Colors.black12,
-    this.iconActiveColor = Colors.black26,
+    bool active: true,
+    this.padding: 10.0,
+    this.iconSize: 25.0,
+    this.activeResizeFactor: 1.0,
+    this.color: Colors.black12,
+    this.iconColor: Colors.black26,
+    Color activeColor,
+    Color iconActiveColor,
+    Color inactiveColor,
+    Color iconInactiveColor,
     this.onPressed,
-  }) : super(key: key);
+  }) : this.active = active == true && onPressed != null,
+       this.activeColor = activeColor ?? color,
+       this.iconActiveColor = iconActiveColor ?? iconColor,
+       this.inactiveColor = inactiveColor ?? color,
+       this.iconInactiveColor = iconInactiveColor ?? iconColor,
+       super(key: key);
 
   final IconData icon;
+  final bool active;
+  final double padding;
+  final double iconSize;
+  final double activeResizeFactor;
   final Color color;
-  final Color iconColor;
   final Color activeColor;
+  final Color iconColor;
   final Color iconActiveColor;
+  final Color inactiveColor;
+  final Color iconInactiveColor;
   final VoidCallback onPressed;
 
   @override
   _RoundedIconButtonState createState() => new _RoundedIconButtonState();
 }
 
-class _RoundedIconButtonState extends State<RoundedIconButton> {
+class _RoundedIconButtonState extends State<RoundedIconButton> with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+  Tween<double> iconSizeAnimation;
+  Tween<double> paddingAnimation;
   bool _tapping = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = new AnimationController(
+      duration: new Duration(milliseconds: 100),
+      vsync: this
+    );
+
+    animation = new CurvedAnimation(
+      parent: animationController,
+      curve: Curves.linear
+    )..addListener(() => setState((){}));
+
+    iconSizeAnimation = new Tween(
+      begin: widget.iconSize,
+      end: widget.iconSize * widget.activeResizeFactor
+    );
+
+    paddingAnimation = new Tween(
+      begin: widget.padding,
+      end: widget.padding * widget.activeResizeFactor
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,14 +81,14 @@ class _RoundedIconButtonState extends State<RoundedIconButton> {
       child: new Container(
         decoration: new BoxDecoration(
           shape: BoxShape.circle,
-          color: _tapping ? widget.activeColor : widget.color,
+          color: widget.active ? (_tapping ? widget.activeColor : widget.color) : widget.inactiveColor,
         ),
         child: new Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: new EdgeInsets.all(paddingAnimation.evaluate(animation)),
           child: new Icon(
             widget.icon,
-            size: 25.0,
-            color: _tapping ? widget.iconActiveColor : widget.iconColor,
+            size: iconSizeAnimation.evaluate(animation),
+            color: widget.active ? (_tapping ? widget.iconActiveColor : widget.iconColor) : widget.iconInactiveColor,
           ),
         )
       )
@@ -50,15 +96,27 @@ class _RoundedIconButtonState extends State<RoundedIconButton> {
   }
 
   void onTapDownHandler(TapDownDetails details) {
-    setState(() => _tapping = true);
+    if (widget.active) {
+      setState(() => _tapping = true);
+      animationController.forward();
+    }
+  }
+
+  void _tapLeave() {
+    setState(() => _tapping = false);
+    animationController.reverse();
   }
 
   void onTapUpHandler(TapUpDetails details) {
-    setState(() => _tapping = false);
-    widget.onPressed();
+    if (widget.active) {
+      _tapLeave();
+      widget.onPressed();
+    }
   }
 
   void onTapCancelHandler() {
-    setState(() => _tapping = false);
+    if (widget.active) {
+      _tapLeave();
+    }
   }
 }
